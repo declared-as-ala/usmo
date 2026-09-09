7fdece374112aa9dee469582989310eb9298ba68
declared-as-ala <alamissaoui.dev@gmail.com>
2026-09-10 00:14:46 +0100

    feat: add Meta Pixel tracking (ID: 2015270885711722)

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -77,6 +77,30 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col antialiased">
         <script
+          dangerouslySetInnerHTML={{
+            __html: `
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '2015270885711722');
+fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=2015270885711722&ev=PageView&noscript=1"
+          />
+        </noscript>
+        <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
             __html: JSON.stringify({
